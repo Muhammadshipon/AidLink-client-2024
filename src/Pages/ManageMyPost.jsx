d31e607e338e14bd6ebe59436673767a0201e4c3
Muhammad <shalauddinahmedshipon@gmail.com>
2024-05-13 15:50:50 -0800

    Manage my post page ui and loading data done

--- src/Pages/ManageMyPost.jsx
+++ src/Pages/ManageMyPost.jsx
@@ -1,105 +1,105 @@
-// import axios from 'axios';
-// import { useContext, useEffect, useState } from 'react';
-// import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-// import 'react-tabs/style/react-tabs.css';
-// import { AuthContext } from '../provider/AuthProvider';
-// import MyNeedVolunteer from '../components/MyNeedVolunteer';
-// import MyVolunteerRequest from '../components/MyVolunteerRequest';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { AuthContext } from '../provider/AuthProvider';
+import MyNeedVolunteer from '../components/MyNeedVolunteer';
+import MyVolunteerRequest from '../components/MyVolunteerRequest';
 
-// const ManageMyPost = () => {
-//   const {user} = useContext(AuthContext);
-//   const [posts,setPosts] = useState([]);
-//   const [requests,setRequests] = useState([]);
+const ManageMyPost = () => {
+  const {user} = useContext(AuthContext);
+  const [posts,setPosts] = useState([]);
+  const [requests,setRequests] = useState([]);
 
 
-//   useEffect(()=>{
-//     const getData = async()=>{
-//       const {data} = await axios(`http://localhost:9000/post?email=${user?.email}`)
-//       setPosts(data);
-//     }
-//    getData();
-//   },[])
-//   console.log(posts);
+  useEffect(()=>{
+    const getData = async()=>{
+      const {data} = await axios(`http://localhost:9000/post?email=${user?.email}`)
+      setPosts(data);
+    }
+   getData();
+  },[])
+  console.log(posts);
 
-//   useEffect(()=>{
-//     const getData = async()=>{
-//       const {data} = await axios(`http://localhost:9000/request?email=${user?.email}`)
-//       setRequests(data);
-//     }
-//    getData();
-//   },[])
-//   console.log(requests)
-//   return (
-//    <div>
-//    <h1 className="text-3xl font-bold text-center my-10 text-yellow-400">Manage My Post</h1>
-// <Tabs>
-//     <TabList>
-//       <Tab >My Need Volunteer Post</Tab>
-//       <Tab >My Volunteer Request Post</Tab>
-//     </TabList>
+  useEffect(()=>{
+    const getData = async()=>{
+      const {data} = await axios(`http://localhost:9000/request?email=${user?.email}`)
+      setRequests(data);
+    }
+   getData();
+  },[])
+  console.log(requests)
+  return (
+   <div>
+   <h1 className="text-3xl font-bold text-center my-10 text-yellow-400">Manage My Post</h1>
+<Tabs>
+    <TabList>
+      <Tab >My Need Volunteer Post</Tab>
+      <Tab >My Volunteer Request Post</Tab>
+    </TabList>
 
-//     <TabPanel>
-//     <table className="table">
-//     {/* head */}
-//     <thead>
-//       <tr>
+    <TabPanel>
+    <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
         
-//         <th>Post title</th>
-//         <th>DeadLine</th>
-//         <th>Location</th>
-//         <th>Update</th>
-//         <th>Delete</th>
-//       </tr>
-//     </thead>
-//     <tbody>
-//       {/* row 1 */}
+        <th>Post title</th>
+        <th>DeadLine</th>
+        <th>Location</th>
+        <th>Update</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
 
-//       {
-//       posts.map(post=><MyNeedVolunteer post={post} key={post._id}></MyNeedVolunteer>)
-//      }
+      {
+      posts.map(post=><MyNeedVolunteer post={post} key={post._id}></MyNeedVolunteer>)
+     }
      
-//     </tbody>
+    </tbody>
    
    
 
-//   </table>
+  </table>
 
 
 
 
 
     
-//     </TabPanel>
-//     <TabPanel>
-//     <table className="table">
-//     {/* head */}
-//     <thead>
-//       <tr>
+    </TabPanel>
+    <TabPanel>
+    <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
         
-//         <th>Request Post Title</th>
-//         <th>DeadLine</th>
-//         <th>Location</th>
-//         <th>Cancel Request</th>
+        <th>Request Post Title</th>
+        <th>DeadLine</th>
+        <th>Location</th>
+        <th>Cancel Request</th>
        
-//       </tr>
-//     </thead>
-//     <tbody>
-//       {/* row 1 */}
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
 
-//       {
-//       requests.map(request=><MyVolunteerRequest request={request} key={request._id}></MyVolunteerRequest>)
-//     }
+      {
+      requests.map(request=><MyVolunteerRequest request={request} key={request._id}></MyVolunteerRequest>)
+    }
      
-//     </tbody>
+    </tbody>
    
    
 
-//   </table>
+  </table>
    
-//     </TabPanel>
-//   </Tabs>
-//    </div>
-//   );
-// };
+    </TabPanel>
+  </Tabs>
+   </div>
+  );
+};
 
-// export default ManageMyPost;
+export default ManageMyPost;
--- src/components/MyNeedVolunteer.jsx
+++ src/components/MyNeedVolunteer.jsx
@@ -1,40 +1,40 @@
 
 
-// const MyNeedVolunteer = ({post}) => {
-//    const {_id,thumbnail,postTitle,location,
-//     category,deadline} = post;
-//   return (
-//     <tr>
+const MyNeedVolunteer = ({post}) => {
+   const {_id,thumbnail,postTitle,location,
+    category,deadline} = post;
+  return (
+    <tr>
         
-//           <td>
-//             <div className="flex items-center gap-3">
-//               <div className="avatar">
-//                 <div className="mask mask-squircle w-12 h-12">
-//                   <img src={thumbnail} alt="Avatar Tailwind CSS Component" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <div className="font-bold">{postTitle}</div>
-//                 <div className="text-sm opacity-50">{category}</div>
-//               </div>
-//             </div>
-//           </td>
+          <td>
+            <div className="flex items-center gap-3">
+              <div className="avatar">
+                <div className="mask mask-squircle w-12 h-12">
+                  <img src={thumbnail} alt="Avatar Tailwind CSS Component" />
+                </div>
+              </div>
+              <div>
+                <div className="font-bold">{postTitle}</div>
+                <div className="text-sm opacity-50">{category}</div>
+              </div>
+            </div>
+          </td>
          
-//           <td>{new Date(deadline).toLocaleDateString()}</td>
+          <td>{new Date(deadline).toLocaleDateString()}</td>
 
-//           <td>
-//             {location}
-//           </td>
-//           <th>
-//           {/* <Link to={`/craft-details/${craft._id}`}></Link> */}
-//           <button className="btn btn-info btn-xs text-white">Update</button>
-//           </th>
-//           <th>
-//           {/* <Link to={`/craft-details/${craft._id}`}></Link> */}
-//           <button className="btn btn-error btn-xs text-white">delete</button>
-//           </th>
-//         </tr>
-//   );
-// };
+          <td>
+            {location}
+          </td>
+          <th>
+          {/* <Link to={`/craft-details/${craft._id}`}></Link> */}
+          <button className="btn btn-info btn-xs text-white">Update</button>
+          </th>
+          <th>
+          {/* <Link to={`/craft-details/${craft._id}`}></Link> */}
+          <button className="btn btn-error btn-xs text-white">delete</button>
+          </th>
+        </tr>
+  );
+};
 
-// export default MyNeedVolunteer;
+export default MyNeedVolunteer;
